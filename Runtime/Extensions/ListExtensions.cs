d9046f5860ad72bb26841a14cdd40ae821f8b76e
Mikita WhaleTee Durunda <n.rob.durunda@gmail.com>
2025-10-15 09:41:54 +0300

    update list extensions

--- Runtime/Extensions/ListExtensions.cs
+++ Runtime/Extensions/ListExtensions.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections.Generic;
 
-namespace WhaleTee.Runtime.Extensions
-{
+namespace WhaleTee.Runtime.Extensions {
   public static class ListExtensions {
     public static void RefreshWith<T>(this List<T> list, IEnumerable<T> items) {
       list.Clear();
       list.AddRange(items);
     }
+
+    public static bool IsNullOrEmpty<T>(this IList<T> list) => list == null || list.Count == 0;
   }
 }
